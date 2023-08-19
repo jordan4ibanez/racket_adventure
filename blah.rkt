aed22e2c2809e5d99715fc3d312d7a5d0c2a7168
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-19 05:14:37 -0400

    Move some stuff into storage

--- blah.rkt
+++ blah.rkt
@@ -35,20 +35,3 @@
 
 (split "hi there")
 (println "")
-
-(define string-args "Create a moonlander out of cheese")
-
-
-(random-seed (modulo (exact-round (+ (string-length string-args) (current-inexact-milliseconds))) 2147483647))
-(let ([choices '(yes no probably "I have no idea" maybe "probably not" "I think so" "Why are you asking me??")])
-  (let ([result (list-ref choices (random 0 (length choices)))])
-    result))
-
-(+ 10 14)
-
-(define (return-multiple)
-  (println "hi")
-  (println "ye")
-  values 1 2 3)
-
-(return-multiple)
